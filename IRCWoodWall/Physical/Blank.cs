f40af1d2d747bbdb17cd5c289ac54e397db83168
AmrFayez <34596347+AmrFayez@users.noreply.github.com>
2018-06-17 22:48:36 +0200

    Revert "cleaned"
    This reverts commit 729d06d83a17e00140b0e2fb00fdb633b519d525.

---
+++ IRCWoodWall/Physical/Blank.cs
@@ -0,0 +1,31 @@
+﻿
+using Bim.Application.IRCWood.Common;
+using Bim.Domain;
+using Bim.Domain.Ifc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bim.Application.IRCWood.Physical
+{
+    enum TimperSections
+    {
+        _2x3,_2x4,_2x6,_2x8,_2x10,_2x12
+    }
+    public abstract class Blank
+    {
+        public double Length { get; set; }
+        public WoodSecType SecType { get; set; }
+        public IfLocation Location { get; set; }
+        protected Orientaion Orientaion { get; set; }
+
+        public Blank()
+        {
+            SecType = WoodSecType._2x4;
+            Location = new IfLocation(0, 0, 0);
+        }
+    }
+}
---
+++ IRCWoodWall/Physical/Plate.cs
@@ -0,0 +1,16 @@
+﻿using Bim.Application.IRCWood.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bim.Application.IRCWood.Physical
+{
+    public class Plate:Blank
+    {
+        public Plate()
+        {
+            Orientaion = Orientaion.FlatHorizontal;
+        }
+    }
+}
