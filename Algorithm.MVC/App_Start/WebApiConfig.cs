07369d54fb9c159f30fd6dcb12c39fa2953d3c5f
AmrFayez <34596347+AmrFayez@users.noreply.github.com>
2018-06-19 06:11:56 +0200

    API configurtion ready

--- Algorithm.MVC/App_Start/WebApiConfig.cs
+++ Algorithm.MVC/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace Algorithm.MVC
@@ -10,6 +11,18 @@ namespace Algorithm.MVC
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
+            // Web API configuration and services
+            config.Formatters.JsonFormatter.Indent = true;
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "json", "application/json")
+                );
+            config.Formatters
+                .JsonFormatter
+                .SerializerSettings
+                .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+         
 
             // Web API routes
             config.MapHttpAttributeRoutes();
