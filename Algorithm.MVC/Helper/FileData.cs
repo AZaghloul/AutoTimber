6277c891d69a84173113914e44a8c567bcbbd5ba
AmrFayez <34596347+AmrFayez@users.noreply.github.com>
2018-06-22 22:40:44 +0200

    Revert "Dashboard2"
    This reverts commit 68d2a941cc58d49cfcef7c060326758151bf0a14.

--- Algorithm.MVC/Helper/FileData.cs
+++ Algorithm.MVC/Helper/FileData.cs
@@ -9,28 +9,24 @@ namespace Algorithm.MVC.Helper
 {
     public struct FileData
     {
-        #region Static Variable
         public static string InputDirectory { get; set; } =
             @"~/Users/input-files/";
         public static string OutputDirectory { get; set; }
         = @"~/Users/output-files/";
-        public static string WexBIMDirectory { get; set; }
-        = @"~/Users/wexbim-files/";
-        #endregion
 
-        #region Properties
-        
+        public static string wexBIMDirectory { get; set; }
+        = @"~/Users/wexbim-files/";
+
+
         public string FileName { get; set; }
 
         public string InputName { get; set; }
         public string OutputName { get; set; }
-        public string WexBIMArcName { get; set; }
-        public string WexBIMStrName { get; set; }
+        public string wexBIName { get; set; }
+
         public string InputPath { get; set; }
         public string OutputPath { get; set; }
-        public string WexBIMPathArc { get; set; }
-        public string WexBIMPAthStr { get; set; }
-        #endregion
+        public string wexBIMPath { get; set; }
         public FileData(string fileName)
         {
             // get File Name To store in DB;
@@ -41,14 +37,14 @@ namespace Algorithm.MVC.Helper
             var ext = Path.GetExtension(FileName);
             var withoutExt = Path.GetFileNameWithoutExtension(FileName);
             OutputName = withoutExt + "- Structure" + ext;
-            WexBIMArcName = withoutExt + ".WexBIM";
-            WexBIMStrName = withoutExt + "- Structure" + ".WexBIM";
+            wexBIName = withoutExt + ".WexBIM";
+
             //get File Path to retrive from directories;
             InputPath = HostingEnvironment.MapPath(InputDirectory + InputName);
             OutputPath = HostingEnvironment.MapPath(OutputDirectory + OutputName);
-            WexBIMPathArc = HostingEnvironment.MapPath(WexBIMDirectory + WexBIMArcName);
-            WexBIMPAthStr = HostingEnvironment.MapPath(WexBIMDirectory + WexBIMStrName);
+            wexBIMPath = HostingEnvironment.MapPath(wexBIMDirectory + wexBIName);
         }
+
         public void FromName(string name)
         {
             FileName = name;
@@ -60,13 +56,12 @@ namespace Algorithm.MVC.Helper
             var ext = Path.GetExtension(FileName);
             var withoutExt = Path.GetFileNameWithoutExtension(FileName);
             OutputName = withoutExt + "- Structure" + ext;
-            WexBIMArcName = withoutExt + ".WexBIM";
-            WexBIMStrName = withoutExt + "- Structure" + ".WexBIM";
+            wexBIName = withoutExt + ".WexBIM";
+
             //get File Path to retrive from directories;
             InputPath = HostingEnvironment.MapPath(InputDirectory + InputName);
             OutputPath = HostingEnvironment.MapPath(OutputDirectory + OutputName);
-            WexBIMPathArc = HostingEnvironment.MapPath(WexBIMDirectory + WexBIMArcName);
-            WexBIMPAthStr = HostingEnvironment.MapPath(WexBIMDirectory + WexBIMStrName);
+            wexBIMPath = HostingEnvironment.MapPath(wexBIMDirectory + wexBIName);
         }
     }
 }
